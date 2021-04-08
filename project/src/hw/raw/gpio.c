299f189160cc30672ce4f882e70ea4481d520293
sjkim <ksj10111011@gmail.com>
2021-04-08 16:41:32 +0900

    rct -> external 오실레이터

--- project/src/hw/raw/gpio.c
+++ project/src/hw/raw/gpio.c
@@ -13,10 +13,10 @@ bool GpioInit(void)
 {
 	bool ret = true;
 
+	__HAL_RCC_GPIOC_CLK_ENABLE();
 	__HAL_RCC_GPIOD_CLK_ENABLE();
 	__HAL_RCC_GPIOA_CLK_ENABLE();
 	__HAL_RCC_GPIOB_CLK_ENABLE();
-
 
 	return ret;
 }
